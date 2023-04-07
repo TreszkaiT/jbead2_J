820ea454ee752ef54285247376b113ef18c2f640
TreszkaiT <treszkaitom@gmail.com>
2023-04-07 08:53:20 +0200

    Init: Jbead2

---
+++ src/main/java/com/tt/jbead/JbeadApplication.java
@@ -0,0 +1,13 @@
+package com.tt.jbead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbeadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JbeadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tt/jbead/JbeadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tt.jbead;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbeadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
