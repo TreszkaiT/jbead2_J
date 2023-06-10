e243b814ff624d954bb978914a5f5ca4bef7cf53
TreszkaiT <treszkaitom@gmail.com>
2023-06-10 07:46:49 +0200

    Feature: add jwt token, but not working yet 03

--- src/main/java/com/tt/jbead/security/login/config/MyUserDetailsService.java
+++ src/main/java/com/tt/jbead/security/login/config/MyUserDetailsService.java
@@ -1,5 +1,7 @@
 package com.tt.jbead.security.login.config;
 
+import com.tt.jbead.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -7,28 +9,31 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 /**
  * Név alapján ki tud kereni egy felhasználót a DB-ből, és azt egy UserDetails objektumba visszaadja, hogy a Spring Security automatikusan tuja ezt kezelni
  */
 @Service
 public class MyUserDetailsService implements UserDetailsService {
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return new User("foo", "foo", new ArrayList<>());
-    }
-}
-
-//    @Autowired
-//    private UserEntityRepository userEntityRepository;
-//
 //    @Override
 //    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//        Optional<UserEntity> userEntity = userEntityRepository.findByUsername(username);
-//
-//        if (userEntity.isPresent()) {
-//            return new AppUserDetails(userEntity.get());
-//        }
-//
-//        throw new UsernameNotFoundException("Not good mate!");
+//        return new User("foo", "foo", new ArrayList<>());
 //    }
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> userEntity = userRepository.findByUsername(username);
+
+        if (userEntity.isPresent()) {
+            return new AppUserDetails(userEntity.get());
+        }
+
+        throw new UsernameNotFoundException("Not good mate!");
+    }
+
+}
+
